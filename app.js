ebf9de183d450be1abc54a5f24c7633b777770ff
PatKo1010 <Pat19951010>
2021-09-13 17:25:31 +0800

    建立密碼產生器專案與環境

---
+++ app.js
@@ -0,0 +1,19 @@
+// set moddules 
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+//set template engine 
+
+app.engine('handlebars', exphbs({defaultLayout: 'main' }))
+app.set ('view engine','handlebars')
+
+//set route 
+app.get ('/', (req, res) => {
+  res.render ('index')
+})
+
+app.listen (port, () => {
+  console.log (`The Express Server will be running on http//locolhost:${port}`)
+})
